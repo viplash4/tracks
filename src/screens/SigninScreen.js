9cdb1a90e9066d3f0183854a38a5475019d38ebb
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2022-03-31 21:08:41 +0300

    signin complet

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import NavLink from '../components/NavLink';
+import AuthForm from '../components/AuthForm';
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+  return (
+    <View style={style.container}>
+      <AuthForm 
+        headerText='Sign in for Trackers'
+        submitButtonText='Sign in'
+        errorMessage=''
+        onSubmit={() => {}}
+      />
+
+      <NavLink
+        text='Dont have an account? Sign up instead'
+        routeName='Signup'
+
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
+  },
+});
 
 export default SigninScreen;
