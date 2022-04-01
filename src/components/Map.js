8e86c811968b0e40ae6ef4064098aed5f47bfc25
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2022-04-01 21:15:18 +0300

    track boilerplate

---
+++ src/components/Map.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, StyleSheet} from 'react-native';
+
+const Map = () => {
+    return <Text>Map</Text>
+};
+
+const style = StyleSheet.create({
+
+});
+
+export default Map;
